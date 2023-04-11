af16631f2f84f872696b2ff6ca89303f8f2a3999
kazi ar rafi <8472627-krafi@users.noreply.gitlab.com>
2023-04-11 09:21:18 +0000

    Update Run-Type-Tiles.py

--- Run-Type-Tiles.py
+++ Run-Type-Tiles.py
@@ -6,73 +6,68 @@ import time
 from pytube import YouTube
 from pydub import AudioSegment
 from pydub.playback import play
-url = "https://www.youtube.com/watch?v=wie68pWg7ss"
-
-
-youtube = YouTube(url)
-audio_stream = youtube.streams.filter(only_audio=True).first()
-audio_file = audio_stream.download(output_path='.', filename='aa')
-
-audio = AudioSegment.from_file(audio_file, format="mp4")
-audio.export("download", format="mp3")
 pygame.init()
 pygame.mixer.init()
-pygame.mixer.music.load("download")
-
-
-pygame.init()
+correct_sound = pygame.mixer.Sound("correct_sound.wav")
+correct_sound.set_volume(0.009)
+incorrect_sound = pygame.mixer.Sound("incorrect_sound.wav")
+incorrect_sound.set_volume(0.1)
+font = pygame.font.SysFont(None, 48)
+input_font = pygame.font.SysFont(None, 24)
 screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Typing Game")
-
-correct_sound = pygame.mixer.Sound("correct_sound.wav")
-correct_sound.set_volume(0.009)
-
-incorrect_sound = pygame.mixer.Sound("incorrect_sound.wav")
-incorrect_sound.set_volume(0.1)
 word_list = ["python", "programming", "game", "typing", "keyboard"]
-
 def choose_word():
     return random.choice(word_list)
-
 def play_music():
+    youtube = YouTube("https://www.youtube.com/watch?v=wie68pWg7ss")
+    audio_stream = youtube.streams.filter(only_audio=True).first()
+    audio_file = audio_stream.download(output_path='.', filename='aa')
+    audio = AudioSegment.from_file(audio_file, format="mp4")
+    audio.export("download", format="mp3")
+    pygame.mixer.music.load("download")
     pygame.mixer.music.play(-1)
-
 def check_input(current_word, input_word):
-    if current_word == input_word:
-        screen.fill((0, 0, 0))
-        return True
-    else:
-        screen.fill((0, 0, 0))
-        return False
+    correct = True
+    surface = font.render(current_word, True, (255, 255, 255))
+    for i, char in enumerate(current_word):
+        if i >= len(input_word) or char != input_word[i]:
+            correct = False
+            if i < len(input_word):
+                surface.blit(input_font.render(input_word[i], True, (255, 0, 0)), (font.size(current_word[:i])[0], 0))
+        else:
+            surface.blit(input_font.render(char, True, (255, 255, 255)), (font.size(current_word[:i])[0], 0))
+    screen.blit(surface, (screen_width/2 - surface.get_width()/2, screen_height/2 - 50))
+    return correct
+
 
 def update_score(correct):
     global score
     if correct:
         score += 1
         correct_sound.play()
+        screen.fill((0, 0, 0))
     else:
         score -= 1
         incorrect_sound.play()
-
+        screen.fill((0, 0, 0), input_font)
 play_music()
 last_input_time = time.time()
 score = 0
-clock = pygame.time.Clock()
-font = pygame.font.SysFont(None, 48)
-input_font = pygame.font.SysFont(None, 24)
 game_over = False
+clock = pygame.time.Clock()
 last_key_time = pygame.time.get_ticks()
 paused = False
-
+incorrect_input = False
 while not game_over:
     current_word = choose_word()
     text = font.render(current_word, True, (255, 255, 255))
     text_rect = text.get_rect(center=(screen_width/2, screen_height/2 - 50))
     screen.blit(text, text_rect)
-
     input_word = ""
+    incorrect_input = False  
     while True:
         event = pygame.event.poll()
         if event.type == pygame.KEYDOWN:
@@ -89,24 +84,32 @@ while not game_over:
         elif event.type == pygame.QUIT:
             game_over = True
             break
-        
-        input_text_surf = input_font.render(input_word, True, (255, 255, 255))
+        incorrect_input = False  
+
+        for i in range(len(input_word)):
+            if i >= len(current_word) or input_word[i] != current_word[i]:
+                incorrect_input = True
+                break
+        if pygame.time.get_ticks() - last_key_time > 2000 and not paused:
+            pygame.mixer.music.pause()
+            paused = True
+        input_text_surf = input_font.render(input_word, True, (255, 0, 0) if incorrect_input else (255, 255, 255))
         input_rect = input_text_surf.get_rect(center=(screen_width/2, screen_height/2 + 50))
         screen.fill((0,0,0), input_rect)
         screen.blit(input_text_surf, input_rect)
         pygame.display.update()
-
-        # Check if it's been more than 2 seconds since the last key press
-        if pygame.time.get_ticks() - last_key_time > 2000 and not paused:
-            pygame.mixer.music.pause()
-            paused = True
-
-    correct = check_input(current_word, input_word)
-    update_score(correct)
+        if input_word == current_word:
+            correct_sound.play()
+            score += 1
+            break
 
     score_text = font.render("Score: " + str(score), True, (255, 255, 255))
+    screen.fill((0, 0, 0))
     screen.blit(score_text, (10, 10))
     pygame.display.update()
+    if score < 0:
+        game_over = True
+        break
     
     if not paused:
         pygame.mixer.music.unpause()
