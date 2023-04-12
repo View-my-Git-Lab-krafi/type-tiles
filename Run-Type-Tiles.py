93b00023a6e1786d10bb2be6b5bbc450779e9e7a
kazi ar rafi <8472627-krafi@users.noreply.gitlab.com>
2023-04-12 09:22:41 +0000

    Update Run-Type-Tiles.py

--- Run-Type-Tiles.py
+++ Run-Type-Tiles.py
@@ -5,6 +5,7 @@ screen_width = 1280
 screen_height = 720
 level = 0
 remove_special_chars = True
+AUTO_PAUSE_NO_TYPE = 2000
 
 # pip install youtube-transcript-api pygame pytube pydub
 # Copyright (c) 2023 Kazi Ar Rafi. All rights reserved.
@@ -13,6 +14,7 @@ remove_special_chars = True
 
 
 ###################################################
+
 from youtube_transcript_api import YouTubeTranscriptApi
 import re
 import pygame
@@ -22,59 +24,37 @@ from pytube import YouTube
 from pydub import AudioSegment
 from pydub.playback import play
 import time
+import os
 
+########################################################
 
-pygame.init()
-pygame.mixer.init()
+correct_sound_video = "https://youtu.be/JQSwIFuqsyM"
+file_path = "correct_sound.wav"
 
-correct_sound = pygame.mixer.Sound("correct_sound.wav")
-correct_sound.set_volume(0.009)
-incorrect_sound = pygame.mixer.Sound("incorrect_sound.wav")
-incorrect_sound.set_volume(0.1)
-font = pygame.font.SysFont(None, 52)
-input_font = pygame.font.SysFont(None, 48)
+if not os.path.exists(file_path):
+    from pytube import YouTube
+    yt = YouTube(correct_sound_video)
+    yt.streams.filter(only_audio=True).first().download()
+    mp4_file = yt.title + ".mp4"
+    sound = AudioSegment.from_file(mp4_file, format="mp4")
+    sound.export(file_path, format="wav")
+sound = AudioSegment.from_wav(file_path)
 
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Typing Game")
-link = ytlink
-video_id = re.findall(r'watch\?v=(\S+)', link)[0]
+###########################################################
 
-try:
-    srt = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
-except:
-    srt = YouTubeTranscriptApi.get_transcript(video_id)
-subtitles_list = []
+incorrect_sound_video = "https://youtu.be/P60PQE-4Lg8"
+file_path = "incorrect_sound.wav"
 
-for line in srt:
-    subtitles_list.append(line["text"])
-subtitles_text = ", ".join(subtitles_list)
+if not os.path.exists(file_path):
+    from pytube import YouTube
+    yt = YouTube(incorrect_sound_video)
+    yt.streams.filter(only_audio=True).first().download()
+    mp4_file = yt.title + ".mp4"
+    sound = AudioSegment.from_file(mp4_file, format="mp4")
+    sound.export(file_path, format="wav")
+sound = AudioSegment.from_wav(file_path)
 
-with open('sub.txt', 'w') as file:
-    file.write(subtitles_text.strip())
-
-word_list = [subtitles_text]
-word_list = subtitles_text.split(", ")
-
-
-if remove_special_chars:
-    word_list = [re.sub(r'[^\w\s]', "", word) for word in word_list]
-
-print(word_list)
-lp = 0
-clock = pygame.time.Clock()
-last_key_time = pygame.time.get_ticks()
-start_time = None
-
-
-def choose_word():
-    global lp
-    tt=(word_list[lp])
-    lp += 1
-
-    if lp >= len(word_list):
-       lp = 0
-    return tt
-
+###########################################################################
 
 def play_music():
     youtube = YouTube(ytlink)
@@ -85,10 +65,63 @@ def play_music():
     pygame.mixer.music.load("download")
     pygame.mixer.music.play(-1)
 
+############################################################################
+
+pygame.init()
+pygame.mixer.init()
+play_music()
+correct_sound = pygame.mixer.Sound("correct_sound.wav")
+correct_sound.set_volume(0.009)
+incorrect_sound = pygame.mixer.Sound("incorrect_sound.wav")
+incorrect_sound.set_volume(0.1)
+#############################################################################
+
+link = ytlink
+video_id = re.findall(r'watch\?v=(\S+)', link)[0]
+try:
+    srt = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
+except:
+    srt = YouTubeTranscriptApi.get_transcript(video_id)
+subtitles_list = []
+for line in srt:
+    subtitles_list.append(line["text"])
+subtitles_text = ", ".join(subtitles_list)
+with open('sub.txt', 'w') as file:
+    file.write(subtitles_text.strip())
+
+word_list = [subtitles_text]
+word_list = subtitles_text.split(", ")
+
+###############################################################################
+
+if remove_special_chars:
+    word_list = [re.sub(r'[^a-zA-Z0-9\s]', "", word) for word in word_list]
+    word_list = [re.sub(r'\s+', " ", word).strip() for word in word_list]
+print(word_list)
+lp = 0
+clock = pygame.time.Clock()
+last_key_time = pygame.time.get_ticks()
+start_time = None
+
+################################################################################
+
+font = pygame.font.SysFont(None, 52)
+input_font = pygame.font.SysFont(None, 48)
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Typing Game")
+def choose_word():
+    global lp
+    tt=(word_list[lp])
+    lp += 1
+    if lp >= len(word_list):
+       lp = 0
+    return tt
+
+#################################################################################
+
 def check_input(current_word, input_word):
     correct = True
     surface = font.render(current_word, True, (80, 250, 123))
-
     for i, char in enumerate(current_word):
         if i >= len(input_word) or char != input_word[i]:
             correct = False
@@ -97,25 +130,20 @@ def check_input(current_word, input_word):
         else:
             surface.blit(input_font.render(char, True, (80, 250, 123)), (font.size(current_word[:i])[0], 0))
     screen.blit(surface, (screen_width/2 - surface.get_width()/2, screen_height/2 - 50))
-
     return correct
 
+#################################################################################
 def update_game(level):
-
     global game_over
     global input_word
 
     wpm = 0
-
     if len(input_word) > 0:
         time_elapsed = time.time() - last_input_time
         wpm = int(len(input_word) / 5 / (time_elapsed / 60))
-
     if wpm < (level * 10):
         game_over = True
-
-
-
+######################################################################################
 def render_game():
     global input_word
 
@@ -126,38 +154,33 @@ def render_game():
     screen.blit(input_text, (10, 50))
 
     pygame.display.update()
-
+#################################################################
 def update_score(correct):
-
     global score
-
     if correct:
         score += 1
         correct_sound.play()
         screen.fill((68, 71, 90))
-
     else:
         score -= 1
         incorrect_sound.play()
         screen.fill((68, 71, 90), input_font)
-
-play_music()
-
+#############################################################
 last_input_time = time.time()
-
 score = 0
 game_over = False
-
 clock = pygame.time.Clock()
 last_key_time = pygame.time.get_ticks()
 paused = False
-
 incorrect_input = False
 first_time = True
-
 #wpm = 0
 
+##############################################################
+
 while not game_over:
+
+    #############################
 
     if first_time == True:
         screen.fill((68, 71, 90))
@@ -168,14 +191,16 @@ while not game_over:
         screen.blit(first_printt, (screen_width / 2 - first_printt.get_width() / 4, screen_height / 5 - first_printt.get_height() / 2))
         first_time = False
 
+    ########################################################
+
     current_word = choose_word()
     text = font.render(current_word, True, (80, 250, 123))
     text_rect = text.get_rect(center=(screen_width/2, screen_height/2 - 50))
     screen.blit(text, text_rect)
-
     input_word = ""
-
     incorrect_input = False  
+
+    ##########################################################
 
     while True:
         event = pygame.event.poll()
@@ -184,30 +209,30 @@ while not game_over:
             paused = False
             if event.key == pygame.K_RETURN:
                 break
-
             elif event.key == pygame.K_BACKSPACE:
                 input_word = input_word[:-1]
                 screen.fill((255, 184, 108), input_rect)
                 pygame.display.update()
-
             else:
                 input_word += event.unicode
-
         elif event.type == pygame.QUIT:
             game_over = True
             break
-
         incorrect_input = False  
+
+        #########################################################
 
         for i in range(len(input_word)):
             if i >= len(current_word) or input_word[i] != current_word[i]:
                 incorrect_input = True
                 break
+        ##########################################################
 
-        if pygame.time.get_ticks() - last_key_time > 2000 and not paused:
+        if pygame.time.get_ticks() - last_key_time > AUTO_PAUSE_NO_TYPE and not paused:
             pygame.mixer.music.pause()
             paused = True
-
+        
+        #################################################################
         time_elapsed = time.time() - last_input_time
         wpm = int(len(input_word) / 5 / (time_elapsed / 60))
         wpm_text = font.render("WPM: " + str(wpm), True, (85, 255, 255))
@@ -216,35 +241,40 @@ while not game_over:
         screen.blit(wpm_text, (screen_width - 150, 10))
         pygame.display.update()
 
+        ##################################################################
+
+
         input_text_surf = input_font.render(input_word, True, (255, 85, 85) if incorrect_input else (80, 250, 123))
         input_rect = input_text_surf.get_rect(center=(screen_width/2, screen_height/2 + 50))
         screen.fill((81, 84, 111), input_rect)
         screen.blit(input_text_surf, input_rect)
         pygame.display.update()
-
+        ####################################################
         if input_word == current_word:
             correct_sound.play()
             score += 1
             break
-
+#####################################################################
 
     time_elapsed = time.time() - last_input_time
     wpm = int(len(input_word) / 5 / (time_elapsed / 60))
     wpm_text = font.render("WPM: " + str(wpm), True, (241, 250, 140))
     update_game(level)
     render_game()
-
+#####################################################################
     score_text = font.render("Score: " + str(score), True, (255, 184, 108))
     screen.fill((68, 71, 90))
     screen.blit(score_text, (10, 10))
     screen.blit(wpm_text, (screen_width - 150, 50)) 
     pygame.display.update()
+########################################################################
     if score < 0:
         game_over = True
         break
-    
+    ##################################################################
     if not paused:
         pygame.mixer.music.unpause()
     last_input_time = time.time()
+    ####################################################################
 pygame.quit()
  
